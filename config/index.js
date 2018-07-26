24468c1aaec4f9a162868fd618c29b2912eb313e
Phil <pespinoza@missiondata.com>
2018-07-25 21:58:03 -0400

    Rename project name

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/home/dist/',
+    assetsPublicPath: '/vue-spa/dist/',
     proxyTable: {},
 
     // Various Dev Server settings
